2d111e4b9ef00f8d39553d258f25e25cd1faf328
Bastian Katz <bastian.katz@hm.edu>
2022-03-08 09:34:13 +0100

    Initializr Output + Libs

---
+++ src/test/java/edu/hm/cs/bka/swt2/helpme/HelpmeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hm.cs.bka.swt2.helpme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
