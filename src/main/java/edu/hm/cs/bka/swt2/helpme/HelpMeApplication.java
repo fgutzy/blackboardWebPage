a99bda2f330dd597ae0c65922c655e159672cec5
Bastian Katz <bastian.katz@hm.edu>
2022-03-12 21:08:11 +0100

    Minor changes.

--- src/main/java/edu/hm/cs/bka/swt2/helpme/HelpMeApplication.java
+++ src/main/java/edu/hm/cs/bka/swt2/helpme/HelpMeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpmeApplication {
+public class HelpMeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelpmeApplication.class, args);
+		SpringApplication.run(HelpMeApplication.class, args);
 	}
 
 }
